e9f0fe14fc03301702dcf246fd7cd595b9c34e28
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-08-25 00:34:05 +0600

    Your commit message here

--- soket-cls-2/index.js
+++ soket-cls-2/index.js
@@ -11,9 +11,31 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+
+
+
+
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
